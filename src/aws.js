b0ec08efd4e4cb7dde215d18d5049a3db21230c8
Badr Larhdir <badrlarhdir7@gmail.com>
2023-06-13 11:37:04 +0200

    Update to gp3 volumetype

--- src/aws.js
+++ src/aws.js
@@ -44,15 +44,15 @@ async function startEc2Instance(label, githubRegistrationToken) {
     IamInstanceProfile: { Name: config.input.iamRoleName },
     TagSpecifications: config.tagSpecifications,
     BlockDeviceMappings: [
-    {
-      DeviceName: '/dev/sda1',
-      Ebs: {
-        VolumeSize: config.input.rootVolumeSize,
-        VolumeType: 'gp2',
-        DeleteOnTermination: true,
+      {
+        DeviceName: '/dev/sda1',
+        Ebs: {
+          VolumeSize: config.input.rootVolumeSize,
+          VolumeType: 'gp3',
+          DeleteOnTermination: true,
+        },
       },
-    },
-  ],
+    ],
   };
 
   try {
